380b4f7da6773e268a890862833e8426c720cbc9
mohmed2000 <noza25588@gmail.com>
2021-03-24 06:22:56 +0200

    init-files
    #1

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+
+function Test(props) {
+    return (
+        <div>
+            <div className="div">
+                <h1>{props.title}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Test
